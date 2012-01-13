a9bb729dcaf2c424fdb632d9ce1ce37e34092cca
Jesse <jesse@brainiac5.(none)>
2012-01-13 11:45:30 -0800

    1D fdfd on gpu works! Praise God!

--- bicg_test.py
+++ bicg_test.py
@@ -5,9 +5,9 @@ import stretched_coords
 
 
 N = 10
-omega = 0.5
-s0 = stretched_coords.get_coeffs(N, 0.0, 1, omega)
-s1 = stretched_coords.get_coeffs(N, 0.5, 1, omega)
+omega = 0.3
+s0 = stretched_coords.get_coeffs(N, 0.0, 2, omega)
+s1 = stretched_coords.get_coeffs(N, 0.5, 2, omega)
 A = np.zeros((N, N))
 for k in range(N):
     A[k,k] = 1
@@ -19,8 +19,6 @@ A1 = np.dot(np.diag(s0), A)
 A2 = np.dot(np.diag(s1), A.T)
 A = np.dot(A2, A1) - omega**2 * (1 - 0.j) * np.eye(N)
 # A = np.dot(A.T, A) - omega**2 * (1 - 0.j) * np.eye(N)
-print A
-print A.T
 def mA(x):
     return np.dot(A, x) 
 
@@ -29,20 +27,24 @@ def mAT(x):
 
 b = np.zeros(N).astype(np.complex128)
 b[N/2+2] = 1 + 0j
+b = np.arange(N).astype(np.complex128)
+# print mA(b)
+print mAT(b)
+
 # print mAT(b)
 
-# Find "exact" solution.
-x = np.linalg.solve(A, b)
-print 'error from "exact" solution', np.linalg.norm(mA(x) - b)
-
-# Solve using bi-cg.
-x = np.zeros_like(b)
-x, err = bicg.solve2(mA, mAT, b, b, x, x, max_iters=1000)
-print len(err), 'iterations'
-print 'ending in:'
-for err_val in err[-5:]:
-    print err_val
-
-print 'error is', np.linalg.norm(mA(x) - b)
+# # Find "exact" solution.
+# x = np.linalg.solve(A, b)
+# print 'error from "exact" solution', np.linalg.norm(mA(x) - b)
+# 
+# # Solve using bi-cg.
+# x = np.zeros_like(b)
+# x, err = bicg.solve2(mA, mAT, b, b, x, x, max_iters=1000)
+# print len(err), 'iterations'
+# print 'ending in:'
+# for err_val in err[-5:]:
+#     print err_val
+# 
+# print 'error is', np.linalg.norm(mA(x) - b)
 # plt.plot(np.abs(x), 'b.-')
 # plt.show()
--- gpuarray_test.py
+++ gpuarray_test.py
@@ -11,12 +11,12 @@ import matplotlib.pyplot as plt
 
 shape = (1, 1, 100)
 omega = 0.3;
-s0 = stretched_coords.get_coeffs(shape[2], 0.0, 2, omega)
+s0 = stretched_coords.get_coeffs(shape[2], 0.0, 10, omega)
 ds0 = ga.zeros(shape, np.complex128) + 1
-s1 = stretched_coords.get_coeffs(shape[2], 0.5, 2, omega)
+s1 = stretched_coords.get_coeffs(shape[2], 0.5, 10, omega)
 ds1 = ga.zeros(shape, np.complex128) + 1
-# ds0.set(s0.astype(np.complex128))
-# ds1.set(s1.astype(np.complex128))
+ds0.set(s0.astype(np.complex128))
+ds1.set(s1.astype(np.complex128))
 env = Environment(loader=PackageLoader(__name__, 'templates'))
 code = env.get_template('d2.cu')
 code1 = Template("""
@@ -68,10 +68,9 @@ def multA(u):
 def multAT(u):
     v0 = ga.empty_like(u)
     v1 = ga.empty_like(u)
-    d2(v0.gpudata, (u * ds1).gpudata, block=shape[::-1], grid=(1,1))
-    d1(v1.gpudata, (v0 * ds0).gpudata, block=shape[::-1], grid=(1,1))
+    d1(v0.gpudata, (u * ds1).gpudata, block=shape[::-1], grid=(1,1))
+    d2(v1.gpudata, (v0 * ds0).gpudata, block=shape[::-1], grid=(1,1))
     return v1 - omega**2 * u
-
 
 x, err = bicg.solve1(multA, multAT, u, v, v*1, max_iters=1000)
 print err.size, 'iterations ending with', err[-3:]
